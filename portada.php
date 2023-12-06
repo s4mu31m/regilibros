31c19274143310f18190f0f201dfd04ae3a403a3
S4MU31M <samu.montiel11@gmail.com>
2023-12-06 16:53:04 -0300

    Update portada.php

--- portada.php
+++ portada.php
@@ -1,41 +1,42 @@
-<?php 
+<?php
 
 require_once 'partials\header.php';
 
 ?>
-  <div class="contenedorP">
+<div class="contenedorP">
 
-    <div class="portada">
-      <h2>Disponibilidad</h2><br>
-      <div class="container">
-         <ul class="list-group">
-          <li class="list-group-dis">Cantidad</li>
-          <li class="list-group-dis">Encuentralo en:</li>
-        </ul>
-      </div>
+  <!--Contenido de la portada-->
+  <div class="portada">
+    <div class="img-Gatsby">
+      <img src="img/gtsb.webp" alt="el.Gatsby">
     </div>
-
-    <!--Contenido de la aportada-->
-    <div class="disponibilidad">
-      <div class="img-Gatsby">
-        <img src="img/elGranGatsby.jpg" alt="el.Gatsby">
-      </div>
-    </div><br><br>
-
-    <!--Contenido de informacion general-->
-    <div class="info-general">
-      <h2>Información general</h2>
-      <br>
-      <ul class="list">
-        <li class="list-group-inf">Autor:</li>
-        <li class="list-group-inf">Editorial:</li>
-      </ul><br>
-      <h3>Sinopsis</h3><br>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi quasi, dicta magnam molestias eos eaque excepturi quo harum beatae consequuntur, totam laudantium adipisci accusamus perferendis nisi voluptatum, impedit nobis natus?
-      </p>
+  </div><br><br>
+  <!--contenido de la disponivilidad-->
+  <div class="disponibilidad">
+    <h2>Disponibilidad</h2><br>
+    <div class="container">
+      <ul class="list-group">
+        <li class="list-group-dis">Cantidad:</li>
+        <li class="list-group-dis">Encuentralo en:</li>
+      </ul>
     </div>
-
   </div>
+
+
+
+
+  <!--Contenido de informacion general-->
+  <div class="info-general">
+    <h2>Información general</h2>
+    <br>
+    <ul class="list">
+      <li class="list-group-inf">Autor:</li>
+      <li class="list-group-inf">Editorial:</li>
+    </ul><br>
+    <h3>Sinopsis</h3><br>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi quasi, dicta magnam molestias eos eaque excepturi quo harum beatae consequuntur, totam laudantium adipisci accusamus perferendis nisi voluptatum, impedit nobis natus?
+    </p>
+  </div>
+
+</div>
 <?php require_once 'partials\footer.php'; ?>
-
-
